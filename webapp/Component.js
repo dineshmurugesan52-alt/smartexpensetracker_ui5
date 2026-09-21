9117bc1b03ee4f9b86ed6ec271f0530327112d5c
Murugesan <dinesh.d.murugesan@accenture.com>
2026-09-21 22:41:58 +0530

    commit

--- webapp/Component.js
+++ webapp/Component.js
@@ -14,7 +14,7 @@ sap.ui.define([
 
         init() {
             // call the base component's init function
-            console.log("Hi");
+            console.log("Hii");
             UIComponent.prototype.init.apply(this, arguments);
 
             // set the device model
